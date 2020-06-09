bb2ad57bee3027ff07259bbe5b3bde8d25ad2697
David Lorite <david.lorite@sysdig.comm>
2020-06-09 18:16:05 +0200

    Fix delete pods

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl version --client
 kubectl -n staging get po
-kubectl -n staging delete po $(kubectl get pods --namespace staging -l "app=backend" -o jsonpath="{.items[0].metadata.name}")
+kubectl -n staging delete po -l app=backend
 kubectl apply -f deployments/kubernetes/backend-deployment.yaml
 kubectl -n staging get po
