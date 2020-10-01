9e11e607d2b3b453a7138dbeaebea661918cf676
David de Torres <david.detorres@sysdig.com>
2020-10-01 12:07:03 +0200

    Added prefix 'v1' to the endpoints of the  API calls

--- web/web_apps_test.go
+++ web/web_apps_test.go
@@ -8,58 +8,81 @@ import (
 )
 
 var _ = Describe("HTTP API for apps", func() {
-	Context("GET /apps", func() {
+	Context("API V1 returns the same than legacy", func() {
+		It("in /apps", func() {
+			responseV1 := doGetRequest("/v1/apps")
+			responseLegacy := doGetRequest("/apps")
+
+			Expect(responseV1).To(Equal(responseLegacy))
+		})
+
+		It("in /apps/:name", func() {
+			responseV1 := doGetRequest("/v1/apps/aws-fargate")
+			responseLegacy := doGetRequest("/apps/aws-fargate")
+
+			Expect(responseV1).To(Equal(responseLegacy))
+		})
+
+		It("in /apps/:name/:appVersion/resources", func() {
+			responseV1 := doGetRequest("/v1/apps/aws-fargate/1.0.0/resources")
+			responseLegacy := doGetRequest("/apps/aws-fargate/1.0.0/resources")
+
+			Expect(responseV1).To(Equal(responseLegacy))
+		})
+	})
+
+	Context("GET /v1/apps", func() {
 		It("returns OK", func() {
-			response := doGetRequest("/apps")
+			response := doGetRequest("/v1/apps")
 
 			Expect(response.StatusCode).To(Equal(http.StatusOK))
 		})
 
 		It("returns an JSON response", func() {
-			response := doGetRequest("/apps")
+			response := doGetRequest("/v1/apps")
 
 			Expect(response.Header.Get("Content-Type"), "application/json")
 		})
 	})
 
-	Context("GET /app/:name", func() {
+	Context("GET /v1/apps/:name", func() {
 		It("returns OK", func() {
-			response := doGetRequest("/apps/aws-fargate")
+			response := doGetRequest("/v1/apps/aws-fargate")
 
 			Expect(response.StatusCode).To(Equal(http.StatusOK))
 		})
 
 		It("returns an JSON response", func() {
-			response := doGetRequest("/apps/aws-fargate")
+			response := doGetRequest("/v1/apps/aws-fargate")
 
 			Expect(response.Header.Get("Content-Type"), "application/json")
 		})
 
 		Context("when name is not found", func() {
 			It("returns a NOTFOUND", func() {
-				response := doGetRequest("/apps/non-existent")
+				response := doGetRequest("/v1/apps/non-existent")
 
 				Expect(response.StatusCode).To(Equal(http.StatusNotFound))
 			})
 		})
 	})
 
-	Context("GET /apps/:name/:appVersion/resources", func() {
+	Context("GET /v1/apps/:name/:appVersion/resources", func() {
 		It("returns OK", func() {
-			response := doGetRequest("/apps/aws-fargate/1.0.0/resources")
+			response := doGetRequest("/v1/apps/aws-fargate/1.0.0/resources")
 
 			Expect(response.StatusCode).To(Equal(http.StatusOK))
 		})
 
 		It("returns an JSON response", func() {
-			response := doGetRequest("/apps/aws-fargate/1.0.0/resources")
+			response := doGetRequest("/v1/apps/aws-fargate/1.0.0/resources")
 
 			Expect(response.Header.Get("Content-Type"), "application/json")
 		})
 
 		Context("when name is not found", func() {
 			It("returns a NOTFOUND", func() {
-				response := doGetRequest("/apps/non-existent/resources")
+				response := doGetRequest("/v1/apps/non-existent/resources")
 
 				Expect(response.StatusCode).To(Equal(http.StatusNotFound))
 			})
@@ -67,7 +90,7 @@ var _ = Describe("HTTP API for apps", func() {
 
 		Context("when app version is not found", func() {
 			It("returns a NOTFOUND", func() {
-				response := doGetRequest("/apps/aws-fargate/1.3/resources")
+				response := doGetRequest("/v1/apps/aws-fargate/1.3/resources")
 
 				Expect(response.StatusCode).To(Equal(http.StatusNotFound))
 			})
